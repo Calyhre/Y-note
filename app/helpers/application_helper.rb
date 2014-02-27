9e33b9e472af06e95ce9b589bdd95b5822a9a560
Calyhre <calyhre@gmail.com>
2014-02-27 16:59:08 +0100

    Add some helper and WIP on a better seed file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def user_avatar
+    image_tag ( current_user.avatar or 'default-avatar.png' ), class: 'avatar'
+  end
+
+  def namespace
+    controller_path.split('/').first
+  end
+
+  def sidebar_link( text:, link:, icon:, classes: '' )
+    class_name = current_page?(link) ? 'current' : ''
+
+    link_to link, class: class_name do
+      content_tag(:i, nil, class: " fa fa-#{icon} #{classes}") + text
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,29 @@
-users = []
-
-50.times do |i|
-  first_name  = Faker::Name.first_name
-  last_name   = Faker::Name.last_name
-  email       = "#{first_name.parameterize}.#{last_name.parameterize}@y-note.com"
-
-  users << User.create( first_name: first_name, last_name: last_name, email: email, password: email, password_confirmation: email )
-end
-
 year = Period.create! title: 'Année 2013-2014', start_at: '30/09/2013', end_at: '11/04/2014'
 
 Period.create! title: 'Trimestre 1', parent: year, start_at: '30/09/2013', end_at: '30/01/2014'
 Period.create! title: 'Trimestre 2', parent: year, start_at: '31/01/2014', end_at: '11/04/2014'
+
+users = 30.times.map do |i|
+  first_name  = Faker::Name.first_name
+  last_name   = Faker::Name.last_name
+  email       = "#{first_name.parameterize}.#{last_name.parameterize}@example.com"
+
+  birthdate = Time.at(25.years.ago + rand * (22.years.ago.to_f - 25.years.ago.to_f)).to_date
+
+  User.create!(
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    password: email,
+    password_confirmation: email,
+    gender: User::GENDERS.sample,
+    birthdate: birthdate
+  )
+end
 
 ing3 = Group.create! title: 'ING3', role: 'student', users: users, period: year
 
 development = Subject.create!( title: 'Development' )
 ruby = Subject.create!( title: 'Ruby', parent: development )
 
-group_subject = GroupSubject.create!( teacher: users.shuffle.first, group: ing3, subject: ruby )
+ruby_ing3 = GroupSubject.create!( teacher: users.shuffle.first, group: ing3, subject: ruby )
